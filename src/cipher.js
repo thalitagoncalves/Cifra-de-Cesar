466530f5ae36c542dd4c5e290b59051919e4ddc2
Thalita Gonçalves <thagsousa15@gmail.com>
2020-04-08 02:11:32 -0300

    Adicionando o documento HTML e CSS alterado. Nos arquivos de JavaScript foram inseridos a parte com a lógica do projeto (exceto a mensagem de erro em caso de null) e a conexão com o HTML.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
 const cipher = {
-  // ...
-};
+  encode: function cifrar(offset, palavra) {
+    let deslocamento = Number(offset);
+    let arr = []; 
+    for(var x = 0; x < palavra.length; x++) {
+      let transf = palavra.toUpperCase().charCodeAt(x);
+      let novaLetra = String.fromCharCode(((transf - 65 + deslocamento) % 26) + 65);
+      if(palavra[x] !== palavra[x].toUpperCase()){
+        novaLetra = novaLetra.toLowerCase();
+      }
+      arr.push(novaLetra);
+    }
+     return arr.join("");
+  },
 
-export default cipher;
+  decode: function decifrar(offset, palavra ){
+    let deslocamento = Number(offset);
+    let arr = [];
+    for(var x = 0; x < palavra.length; x++) {
+      let transf = palavra.toUpperCase().charCodeAt(x);
+      let novaLetra = String.fromCharCode(((transf + 65 - deslocamento) % 26) + 65  );
+      if(palavra[x] !== palavra[x].toUpperCase()){
+        novaLetra = novaLetra.toLowerCase();
+      }
+      arr.push(novaLetra);
+    }
+     return arr.join("");
+  }
+
+}
+
+
+
+export default cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+let enviarCifragem = document.getElementById("cifragem");
+function cifrar() {
+    let palavra = document.getElementById("palavra-cifrada").value;
+    let offset = document.getElementById("offset1").value;
+    document.getElementById("mostrar-mensagem").innerHTML = cipher.encode(offset, palavra);
+
+}
+
+enviarCifragem.addEventListener("click", cifrar);
+
+let enviarDecifragem = document.getElementById("decifragem");
+function decifrar() {
+    let palavra = document.getElementById("palavra-decifrada").value;
+    let offset = document.getElementById("offset2").value;
+    document.getElementById("mostrar-mensagem2").innerHTML = cipher.decode(offset, palavra);
+
+}
+
+enviarDecifragem.addEventListener("click", decifrar);
+
+
+
